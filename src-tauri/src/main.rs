46c8dadab6e9dce8ad2aa44906dedf873033935e
tyza66 <shun_@outlook.com>
2026-08-31 20:05:44 +0800

    docs: 源码注释中加入 FasterEdge 组织标识与 GitHub/Gitee 链接

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,4 +1,5 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+// FasterEdge 开源项目 · https://github.com/FasterEdge · https://gitee.com/FasterEdge
 
 fn main() {
     simplewebshell_desktop_lib::run();
